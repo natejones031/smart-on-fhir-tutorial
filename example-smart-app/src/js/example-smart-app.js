800378570ec6319d269fd72cd9076e661decba79
natejones031 <natejones031@gmail.com>
2019-10-19 17:47:17 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -36,8 +36,8 @@
             lname = patient.name[0].family.join(' ');
           }
 
-          var weight = byCodes('29463-7');
           var height = byCodes('8302-2');
+          var weight = byCodes('29463-7');
           var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
@@ -81,8 +81,8 @@
       lname: {value: ''},
       gender: {value: ''},
       birthdate: {value: ''},
-      weight: {value: ''},
       height: {value: ''},
+      weight: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
       ldl: {value: ''},
