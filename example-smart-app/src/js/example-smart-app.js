b22d33cbe406885069eb59bfed01dbd2c20a76f1
natejones031 <natejones031@gmail.com>
2019-10-19 17:17:29 -0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -78,6 +78,7 @@
       lname: {value: ''},
       gender: {value: ''},
       birthdate: {value: ''},
+      weight: {value: ''},
       height: {value: ''},
       systolicbp: {value: ''},
       diastolicbp: {value: ''},
@@ -122,6 +123,7 @@
     $('#gender').html(p.gender);
     $('#birthdate').html(p.birthdate);
     $('#height').html(p.height);
+    $('#weight').html(p.weight);
     $('#systolicbp').html(p.systolicbp);
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
